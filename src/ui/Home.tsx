ce55958ec4e1efca56722732476926fa9de99600
Quekzbabiez <154868358+Quekzbabiez@users.noreply.github.com>
2023-12-26 08:04:54 +0100

    Update Home.tsx

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '../components/Button'
+import { NavButton } from '../components/Button'
 import { Section } from '../components/Section'
 import { Icon } from '../components/Icon'
 import styles from './Home.module.css'
@@ -15,15 +15,13 @@ export function Home() {
           An open source, decentralized casino built on Gamba. Copy this platform with your own branding and start earning fees on every bet.
         </p>
         <div style={{ display: 'flex', gap: '10px' }}>
-          <Button
-            as="a"
+          <NavButton
             color="white"
-            href="https://gamba.so"
-            target="_blank"
+            to="/flip"
             icon={<Icon.ExternalLink />}
           >
-            Learn more
-          </Button>
+            Play Flip
+          </NavButton>
         </div>
       </Section>
     </div>
